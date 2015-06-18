92f1902eff49ae35066351826c51a71b1dd5851b
ben <ben@ubuntu2>
2015-06-18 18:21:09 +0200

    activate InputCorrectAnswer

--- src/bluejexercisecheck/BlueJExerciseCheckController.java
+++ src/bluejexercisecheck/BlueJExerciseCheckController.java
@@ -1,8 +1,6 @@
 package bluejexercisecheck;
 
-import bluejexercisecheck.BlueJExerciseCheckViewInputQuestion;
-import bluejexercisecheck.BlueJExerciseCheckView;
-import bluejexercisecheck.BlueJExerciseCheckModel;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
@@ -11,8 +9,8 @@ import javax.swing.JFrame;
 
 public class BlueJExerciseCheckController {
 
-	private BlueJExerciseCheckView theView;
-	private BlueJExerciseCheckModel theModel;
+	private final BlueJExerciseCheckView theView;
+	private final BlueJExerciseCheckModel theModel;
 	
 
 	public BlueJExerciseCheckController(BlueJExerciseCheckView theView, BlueJExerciseCheckModel theModel)
@@ -24,7 +22,7 @@ public class BlueJExerciseCheckController {
 		this.theView.addSaveActionListener(new InputQuestionListener());
 		this.theView.addNextActionListener(new InputAnswerListener());
 		// get values out of db and set in the view
-		this.theModel.setConnectionDatabase();
+		//this.theModel.setConnectionDatabase();
 		
 	}
 
@@ -44,7 +42,7 @@ public class BlueJExerciseCheckController {
 			theViewInputQuestion.setVisible(true);
 			System.out.println("Question Clicked!");
 		}	 
-			// Next button Listener
+			// Save button Listener
 				class SaveBtnListener implements ActionListener {
 
 					@Override
@@ -84,16 +82,55 @@ public class BlueJExerciseCheckController {
 
 	// Next button Listener
 	class InputAnswerListener implements ActionListener {
+            private BlueJExerciseCheckViewInputCorrectAnswer theViewInputCorrectAnswer;
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-					
+			theViewInputCorrectAnswer = new BlueJExerciseCheckViewInputCorrectAnswer();
+                        theViewInputCorrectAnswer.addSaveActionListener(new SaveBtnListener());
+			theViewInputCorrectAnswer.addNextActionListener(new NextBtnListener());
+			theViewInputCorrectAnswer.addPreviousActionListener(new PreviousBtnListener());
+			theViewInputCorrectAnswer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			theViewInputCorrectAnswer.setSize(400, 600);
+			theViewInputCorrectAnswer.setVisible(true);
 
 			System.out.println("Correct Answer Clicked!");
+                }       
+                        // Save button Listener
+				class SaveBtnListener implements ActionListener {
+
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+								
+						System.out.println("save Clicked!");
+
+					}
+
+				}
+				class NextBtnListener implements ActionListener {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						System.out.println("next  Clicked!");
+
+					}
+					
+				}
+				class PreviousBtnListener implements ActionListener{
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("previous Clicked!");
+
+					}
+					
+				}
+
 
 		}
 
-	}
+	
 
 	
 
