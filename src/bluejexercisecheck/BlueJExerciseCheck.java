46c22a0293ef2b97850a1a5386a1652c5c27a37f
badev <badev@localhost>
2015-09-29 15:39:59 +0200

    FrameByeBye removed

--- src/bluejexercisecheck/BlueJExerciseCheck.java
+++ src/bluejexercisecheck/BlueJExerciseCheck.java
@@ -1,15 +1,6 @@
 package bluejexercisecheck;
 
-import bluejexercisecheck.BlueJExerciseCheckView;
-import bluejexercisecheck.BlueJExerciseCheckModel;
-import java.awt.Component;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.sql.SQLException;
-
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 
 
 public class BlueJExerciseCheck 
@@ -27,38 +18,3 @@ public class BlueJExerciseCheck
     }
 }
 
-class FrameByeBye 
-{
-    // The method we wish to call on exit.
-    public static void showDialog(Component c) 
-    {
-        JOptionPane.showMessageDialog(c, "Bye Bye!");
-    }
-
-    public static void main(String[] args) 
-    {
-        // creating/udpating Swing GUIs must be done on the EDT.
-        SwingUtilities.invokeLater( new Runnable() 
-        {
-            public void run() 
-            {
-                final JFrame f = new JFrame("Say Bye Bye!");
-                // Swing's default behavior for JFrames is to hide them.
-                f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-                f.addWindowListener( new WindowAdapter() 
-                {
-                    @Override
-                    public void windowClosing(WindowEvent we) 
-                    {
-                        showDialog(f);
-                        System.exit(0);
-                    }
-                } );
-                
-                f.setSize(300,200);
-                f.setLocationByPlatform(true);
-                f.setVisible(true);
-            }
-        } );
-    }
-}// end class FrameByeBye
--- src/bluejexercisecheck/BlueJExerciseCheckController.java
+++ src/bluejexercisecheck/BlueJExerciseCheckController.java
@@ -1,6 +1,5 @@
 package bluejexercisecheck;
 
-import static bluejexercisecheck.FrameByeBye.showDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
