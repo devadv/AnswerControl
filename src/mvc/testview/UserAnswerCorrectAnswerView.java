ce2b1468b33715488ebff7a2bbb47f30c29286dd
hintveld <hintveld@localhost>
2018-01-08 11:59:31 +0100

    add two panels to UserAnswerCorrectAnswerView

--- src/mvc/testview/UserAnswerCorrectAnswerView.java
+++ src/mvc/testview/UserAnswerCorrectAnswerView.java
@@ -1,8 +1,8 @@
 package mvc.testview;
 
 import java.awt.BorderLayout;
+import java.awt.Insets;
 import java.util.Observable;
-
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,26 +26,56 @@ public class UserAnswerCorrectAnswerView extends AbstractView{
         this.setLocation(500, 150);
         this.setVisible(true);
 		setComponents();
-		answerLabel = new JLabel("Answer:");
-		correctAnswerLabel = new JLabel("Correct answer:");
+		blockBox.setSelectedItem(blockId);
 
 		JPanel answerPanel = new JPanel(new BorderLayout());
+		answerLabel = new JLabel("Your answer:");
 		answer = new JTextArea(30, 40);
+		answer.setLineWrap(true);
+		answer.setWrapStyleWord(true);
+		answer.setMargin(new Insets(5, 10, 0, 5));
 		JScrollPane scrollPane = new JScrollPane(answer);
 		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		answerPanel.setBorder(BorderFactory.createEmptyBorder(10, 37, 10, 37));
-		answerPanel.add(scrollPane, BorderLayout.WEST);
+		answerPanel.add(scrollPane, BorderLayout.CENTER);
+		answerPanel.add(answerLabel, BorderLayout.NORTH);
 
+		JPanel correctAnswerPanel = new JPanel(new BorderLayout());
+		correctAnswerLabel = new JLabel("Correct answer:");
 		correctAnswer = new JTextArea(30, 40);
+		correctAnswer.setLineWrap(true);
+		correctAnswer.setWrapStyleWord(true);
 		JScrollPane scrollPane1 = new JScrollPane(correctAnswer);
 		scrollPane1.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane1.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		answerPanel.setBorder(BorderFactory.createEmptyBorder(10, 37, 10, 37));
-		answerPanel.add(scrollPane1, BorderLayout.EAST);
+		correctAnswerPanel.setBorder(BorderFactory.createEmptyBorder(10, 37, 10, 37));
+		correctAnswerPanel.add(scrollPane1, BorderLayout.CENTER);
+		correctAnswerPanel.add(correctAnswerLabel, BorderLayout.NORTH);
 
-		panelCenter.add(answerPanel);
-		blockBox.setSelectedItem(blockId);
+		panelCenter.add(answerPanel, BorderLayout.WEST);
+		panelCenter.add(correctAnswerPanel, BorderLayout.EAST);
+		setAnswerAndCorrectAnswer();
+		updateView();
+	}
+
+
+
+	@Override
+	public void btnNext() {
+		super.btnNext();
+		setAnswerAndCorrectAnswer();
+	}
+
+	@Override
+	public void btnPrevious() {
+		super.btnPrevious();
+		setAnswerAndCorrectAnswer();
+	}
+
+	public void setAnswerAndCorrectAnswer() {
+		answer.setText(model.retrieveAnswerUser(getExerciseNr(), System.getProperty("user.name")));
+		correctAnswer.setText(model.retrieveAnswer(getExerciseNr()));
 	}
 
 
