34a02480e9659709b6c74e9546063d618d5f5d9e
hintveld <hintveld@localhost>
2018-01-24 11:41:13 +0100

    set main to ControllerInputUserAnswer

--- src/mvc/main/Main.java
+++ src/mvc/main/Main.java
@@ -1,14 +1,16 @@
 package mvc.main;
 
 import mvc.controller.ControllerInputQuestionAnswerView;
+import mvc.controller.ControllerInputUserAnswer;
+import mvc.controller.ControllerSave;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		//ControllerSave controllerSave = new ControllerSave();
-		//ControllerInputUserAnswer controllerInputUser = new ControllerInputUserAnswer();
-		ControllerInputQuestionAnswerView controllerInputQuestionAnswerView = new ControllerInputQuestionAnswerView();
+		ControllerInputUserAnswer controllerInputUser = new ControllerInputUserAnswer();
+		//ControllerInputQuestionAnswerView controllerInputQuestionAnswerView = new ControllerInputQuestionAnswerView();
 	}
 
 }
